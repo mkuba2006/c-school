ad41db90fb1618100c5b2fc1ca95f9fd58a24157
Jakub Myrta <mkuba2006@gmail.com>
2023-05-08 14:15:38 +0200

    removed1

--- mic.cpp
+++ mic.cpp
@@ -1,30 +1,4 @@
-#include
-#include
 
-using namespace std;
-
-int main()
-{
-string t_jawny, szyfrogram = "";
-int i, klucz;
-int kod;
-
-cout << "Tekst jawny: ";
-cin >> t_jawny;
-
-cout << "Klucz: ";
-cin >> klucz;
-
-for(i = 0; i < t_jawny.size(); i++)
-{
-    kod = (t_jawny[i] + klucz - 'A') % 26 + 'A';
-    szyfrogram = szyfrogram + char(kod);
-}
-
-cout << "Szyfrogram: " << szyfrogram;
-
-return 0;
-}
 ////////////////////////////////////////////////////////////////////
 #include
 #include
